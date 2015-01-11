e90101c664298cf18310a4ee914efe793720c761
Corina Usher <Corina.Usher@barclayscapital.com>
2015-01-11 20:08:11 +0000

    Updated to use BigDecimal

--- src/main/scala/PricingService.scala
+++ src/main/scala/PricingService.scala
@@ -3,20 +3,21 @@ import scala.annotation.tailrec
 object PricingService {
   type Basket = List[Int]
 
-  def price(basket: Basket): Double = {
+  def price(basket: Basket): BigDecimal = {
     def discountedPrice(numberOfDifferentBooks: Int): BigDecimal = numberOfDifferentBooks match {
-      case 5 => price * 0.75
-      case 4 => price * 0.8
-      case 3 => price * 0.9
-      case 2 => price * 0.95
-      case _ => price * 1.0
+      case 5 => price * BigDecimal(0.75)
+      case 4 => price * BigDecimal(0.8)
+      case 3 => price * BigDecimal(0.9)
+      case 2 => price * BigDecimal(0.95)
+      case _ => price * BigDecimal(1.0)
     }
-
-    totUp(0, 0, basket, basket.toSet.size, discountedPrice(basket.toSet.size))
+//list.filterNot(list)
+    val numberOfDifferentBooks: Int = basket.distinct.size
+    totUp(0, 0, basket, numberOfDifferentBooks, discountedPrice(numberOfDifferentBooks))
   }
 
   @tailrec
-  def totUp(acc: Int, runningTotal: BigDecimal, basket: Basket, numberOfDifferentBooks: Int, reducedPrice: BigDecimal): Double = {
+  def totUp(acc: Int, runningTotal: BigDecimal, basket: Basket, numberOfDifferentBooks: Int, reducedPrice: BigDecimal): BigDecimal = {
     def getPrice: BigDecimal = if (numberOfDifferentBooks - acc > 0) reducedPrice else price
 
     if (basket.isEmpty)
@@ -24,11 +25,11 @@ object PricingService {
     else {
       totUp(acc + 1, runningTotal + getPrice, basket.tail, numberOfDifferentBooks, reducedPrice)
     }
-}
+  }
 
-def format(runningTotal: BigDecimal): Double = {
-  runningTotal.setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
-}
+  def format(runningTotal: BigDecimal): BigDecimal = {
+    runningTotal.setScale(2, BigDecimal.RoundingMode.HALF_UP)
+  }
 
-private val price: BigDecimal = 8
+  private val price: BigDecimal = 8
 }
