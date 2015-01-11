fcc1e0729530867a0590c6c5e6ca55f93962e09f
Corina Usher <Corina.Usher@barclayscapital.com>
2015-01-11 22:52:46 +0000

    Refactored to allow us to group books into distinct sets to apply multiple discounts

--- src/main/scala/PricingService.scala
+++ src/main/scala/PricingService.scala
@@ -2,34 +2,44 @@ import scala.annotation.tailrec
 
 object PricingService {
   type Basket = List[Int]
+  type GroupsOfBooks = List[List[Int]]
+  type Combinations = List[List[Int]]
 
   def price(basket: Basket): BigDecimal = {
-    def discountedPrice(numberOfDifferentBooks: Int): BigDecimal = numberOfDifferentBooks match {
-      case 5 => price * BigDecimal(0.75)
-      case 4 => price * BigDecimal(0.8)
-      case 3 => price * BigDecimal(0.9)
-      case 2 => price * BigDecimal(0.95)
-      case _ => price * BigDecimal(1.0)
-    }
-//list.filterNot(list)
-    val numberOfDifferentBooks: Int = basket.distinct.size
-    totUp(0, 0, basket, numberOfDifferentBooks, discountedPrice(numberOfDifferentBooks))
+    val possibleGroupings = getGroupOfBooks(Nil, basket)
+    possibleGroupings.foldLeft(BigDecimal(0))((total, nextGroup) => total + priceWithDiscounts(nextGroup) )
   }
 
   @tailrec
-  def totUp(acc: Int, runningTotal: BigDecimal, basket: Basket, numberOfDifferentBooks: Int, reducedPrice: BigDecimal): BigDecimal = {
-    def getPrice: BigDecimal = if (numberOfDifferentBooks - acc > 0) reducedPrice else price
-
-    if (basket.isEmpty)
-      format(runningTotal)
-    else {
-      totUp(acc + 1, runningTotal + getPrice, basket.tail, numberOfDifferentBooks, reducedPrice)
+  def getGroupOfBooks(acc: GroupsOfBooks, remainingBasket: Basket): GroupsOfBooks = {
+    if (remainingBasket.isEmpty)
+      acc
+    else{
+      val nextGroup = getPossibleGroupings(remainingBasket).head
+      val newRemainingBasket = remainingBasket diff nextGroup
+      getGroupOfBooks(nextGroup:: acc, newRemainingBasket)
     }
   }
 
-  def format(runningTotal: BigDecimal): BigDecimal = {
-    runningTotal.setScale(2, BigDecimal.RoundingMode.HALF_UP)
+  def getPossibleGroupings(basket: Basket): List[List[Int]] = {
+    val allGroupings = for {
+      i <- 1 to basket.distinct.length
+      combinations <- basket combinations i
+    } yield combinations
+    allGroupings.filter(group => group.size == group.distinct.size).toList
   }
 
-  private val price: BigDecimal = 8
+  def priceWithDiscounts(basket: List[Int]): BigDecimal = {
+    val noOfDifferentBooks: Int = basket.size
+    val discount = noOfDifferentBooks match {
+      case 5 => .75
+      case 4 => .8
+      case 3 => .9
+      case 2 => .95
+      case _ => 1
+    }
+    price * noOfDifferentBooks * discount
+  }
+
+  private val price = BigDecimal(8)
 }
--- src/test/scala/AcceptanceTest.scala
+++ src/test/scala/AcceptanceTest.scala
@@ -1,60 +1,66 @@
-
 class AcceptanceTest extends org.scalatest.FunSuite {
-  test("no book costs nothing") {
+  test("An empty basket costs nothing") {
     assert(0 === PricingService.price(Nil))
   }
 
-  test("any one book costs 8€") {
+  test("A basket containing any one book costs 8€") {
     assert(8 === PricingService.price(0 :: Nil))
+    assert(8 === PricingService.price(1 :: Nil))
+    assert(8 === PricingService.price(2 :: Nil))
+    assert(8 === PricingService.price(3 :: Nil))
+    assert(8 === PricingService.price(4 :: Nil))
   }
 
-  test("n copies of the same book costs n times the cost of one book (i.e. no discounts)") {
+  test("A basket containing several copies of the same book receives no discount") {
     assert(16 === PricingService.price(0 :: 0 :: Nil))
-    assert(24 === PricingService.price(0 :: 0 :: 0 :: Nil))
-    assert(32 === PricingService.price(0 :: 0 ::0 :: 0 :: Nil))
-    assert(40 === PricingService.price(0 :: 0 :: 0 :: 0 :: 0 :: Nil))
-    assert(48 === PricingService.price(0 :: 0 :: 0 :: 0 :: 0 :: 0 :: Nil))
   }
 
-  test("If you buy all 5 books you get a 25% discount") {
-    val basket: List[Int] = 0 :: 1 :: 2 :: 3 :: 4 :: Nil
+  test("A basket of all 5 books receives a 25% discount") {
+    val basket = 0 :: 1 :: 2 :: 3 :: 4 :: Nil
     assert(30 === PricingService.price(basket))
   }
 
-  test("If you buy 4 different books you get a 20% discount") {
-    val basket: List[Int] = 0 :: 1 :: 2 :: 3 :: Nil
+  test("A basket of 4 different books receives a 20% discount") {
+    val basket = 0 :: 1 :: 2 :: 3 :: Nil
     assert(25.60 === PricingService.price(basket))
   }
 
-  test("If you buy 3 different books you get a 10% discount") {
-    val basket: List[Int] = 0 :: 1 :: 2 :: Nil
+  test("A basket of 3 different books receives a 10% discount") {
+    val basket = 0 :: 1 :: 2 :: Nil
     assert(21.60 === PricingService.price(basket))
   }
 
-  test("If you buy 2 different books you get a 5% discount") {
-    val basket: List[Int] = 0 :: 1 :: Nil
+  test("A basket of 2 different books receives a 5% discount") {
+    val basket = 0 :: 1 :: Nil
     assert(15.20 === PricingService.price(basket))
   }
 
-  test("If you buy 4 books, 3 of which are different titles, you get a 10% discount on those 3 but the fourth book still costs 8€"){
-    val basket: List[Int] = 0 :: 0 :: 1 :: 2 :: Nil
-    assert(29.60 === PricingService.price(basket))
-  }
-
-  test("If you buy 3 books, 2 of which are different titles, you get a 5% discount on those 2 but the third book still costs 8€"){
-    val basket: List[Int] = 0:: 0 :: 1 :: Nil
+  test("Discounts only apply to a set of different books") {
+    val basket = 0 :: 0 :: 1 :: Nil
     assert(23.20 === PricingService.price(basket))
   }
 
-  test("If you buy 4 books, 2 of which are different titles, you get a 5% discount on 2 books but the other two book still cost 8€ each"){
-    val basket: List[Int] = 0 :: 0 :: 1 :: 1 :: Nil
-    assert(31.20 === PricingService.price(basket))
+  test("Group books into multiple sets to each attract a discount") {
+    assert(30.40 === PricingService.price(0 :: 1 :: 0 :: 1 :: Nil))
+   // assert(30.40 === PricingService.price(0 :: 0 :: 1 :: 1 :: Nil))
   }
 
-//  test("If you buy 6 books, 4 of which are different titles, you can group such that you get a 20% discount on 4 and a 5% discount on two"){
-//    val basket: List[Int] = 0 :: 0 :: 1 :: 2 :: 2 :: 3 :: Nil
-//    assert(40.80 === PricingService.price(basket))
-//  }
+  //  test("If you buy 4 books, 3 of which are different titles, you get a 10% discount on those 3 but the fourth book still costs 8€"){
+  //    val basket: List[Int] = 0 :: 0 :: 1 :: 2 :: Nil
+  //    assert(29.60 === PricingService.price(basket))
+  //  }
+  //
 
- // assert_equal(8 + (8 * 5 * 0.75), price([0, 1, 1, 2, 3, 4]))
+  //
+  //  test("If you buy 4 books, 2 of which are different titles, you get a 5% discount on 2 books but the other two book still cost 8€ each"){
+  //    val basket: List[Int] = 0 :: 0 :: 1 :: 1 :: Nil
+  //    assert(31.20 === PricingService.price(basket))
+  //  }
+
+  //  test("If you buy 6 books, 4 of which are different titles, you can group such that you get a 20% discount on 4 and a 5% discount on two"){
+  //    val basket: List[Int] = 0 :: 0 :: 1 :: 2 :: 2 :: 3 :: Nil
+  //    assert(40.80 === PricingService.price(basket))
+  //  }
+
+  // assert_equal(8 + (8 * 5 * 0.75), price([0, 1, 1, 2, 3, 4]))
 }
